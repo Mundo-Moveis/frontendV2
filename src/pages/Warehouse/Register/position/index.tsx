9c15d7cad2ca3ee690b5d9f63bcc27aecdb5fd77
SuddoBruno <brunoleonardo.profissional@gmail.com>
2021-07-07 17:37:18 -0300

    iniciando positon

---
+++ src/pages/Warehouse/Register/position/index.tsx
@@ -0,0 +1,35 @@
+import {
+  DeleteOutlined,
+  EditFilled,
+  PlusOutlined,
+  SearchOutlined,
+} from '@ant-design/icons';
+import {
+  Button,
+  Col,
+  Form,
+  Input,
+  Layout,
+  Modal,
+  Popconfirm,
+  Row,
+  Select,
+  Space,
+  Table,
+} from 'antd';
+import React, { FormEvent, useState } from 'react';
+import Highlighter from 'react-highlight-words';
+import styles from './styles/style.module.scss';
+
+import { Notification } from '../../../../components/Notification';
+import { api } from '../../../../services/api';
+import { GetServerSideProps } from 'next';
+
+const { Option } = Select;
+
+interface Position {}
+
+export default function position() {
+  return;
+  <div></div>;
+}
